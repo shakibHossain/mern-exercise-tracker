85485fa8651fd8edaf6fd17c177ee70d67e2d09d
shakibHossain <shakib.hossain.sh@gmail.com>
2020-08-19 15:45:02 -0600

    update .env file to make uri a string

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ mongoose.connect(uri, {
 });
 const connection = mongoose.connection;
 connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfullyy");
 });
 
 // Routes
